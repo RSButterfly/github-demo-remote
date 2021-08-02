ff60f16b7f873174003a9390ce54ca0e7b4f86dd
sutanu_sil <sutanu_sil@persistent.com>
2021-08-02 14:36:01 +0530

    fn header modified and squre fn deleted

--- math.py
+++ math.py
@@ -2,21 +2,17 @@
 def add(x,y):
     return x+y
 
-#Sub implementation
+#Subtract implementation
 def subtract(x,y):
     return x-y         # on master branch
 
-#Mul implementation
+#Multiply implementation
 def multiply(x,y):
     return x*-y         # on branch Bug456
 
-#Div implementation
+#Divide implementation
 def divide(x,y):
     if y==0:                        # on master branch
         return DIVIDE_BY_0_ERROR
     else:
         return x/y       
-
-# square implementation
-def square(x):
-    return x*x
